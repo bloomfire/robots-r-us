d4faf53e3562c9dd01a46007529edb081e40157e
Kimberly Bui <kimberlybui@Kimberlys-MacBook-Pro.local>
2020-10-17 00:39:48 -0500

    robots-r-us-1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,48 @@ import React from 'react';
 import data from './data';
 import './App.css';
 
-function App() {
+class Follow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'Follow' : 'Unfollow'}
+      </button>
+    );
+  }
+}
+export default function App() {
+  const allMapped = data.map((robot) => {
+    
+  return (
+    <div className="card">
+      <img className="avatar" src={robot.avatar}/>
+      {/* <div className="id">{robot.id}</div> */}
+      <div className="name">{robot.first_name} {robot.last_name}</div>
+      <Follow/>
+      <div className="title">{robot.title}</div>
+      <div className="email">{robot.email}</div>
+    </div>
+ );
+});
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      <h1 className="header">Robots-R-Us</h1>
+     
+      <div className="grid-container">{allMapped}</div>
+  
     </div>
+    
   );
 }
 
-export default App;
