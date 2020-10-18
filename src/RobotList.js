553b7653662df54f806acab79ce21e091ce26c99
andrewbinkard <andrewbinkard@gmail.com>
2020-10-18 13:39:48 -0500

    begin styling react-bootstrap cards

--- src/RobotList.js
+++ src/RobotList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import data from "./data";
+import { Card } from "react-bootstrap";
 import "./App.css";
 
 const RobotList = () => {
@@ -7,16 +8,27 @@ const RobotList = () => {
     <div className="App">
       {data.map((robot) => {
         return (
-          <div key={robot.id}>
-            <div>
-              {robot.first_name} {robot.last_name}
-            </div>
+          <div key={robot.id} className="robotCard">
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>
+                  <div className="nameText">
+                    {robot.first_name} {robot.last_name}
+                  </div>
+                </Card.Title>
+                <Card.Img
+                  variant="bottom"
+                  src={
+                    robot.avatar
+                      ? robot.avatar
+                      : "https://i.imgur.com/CsQIkSE.jpg"
+                  }
+                />
+                {/* <Button variant="primary">Go somewhere</Button> */}
+              </Card.Body>
+            </Card>
+
             <div>{robot.email}</div>
-            <img
-              src={
-                robot.avatar ? robot.avatar : "https://i.imgur.com/CsQIkSE.jpg"
-              }
-            ></img>
             <div>{robot.title ? robot.title : "No Purpose Yet Defined"}</div>
           </div>
         );
