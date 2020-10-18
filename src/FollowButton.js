72e2c6b9f08473589a1316ee551d057ce013aa22
andrewbinkard <andrewbinkard@gmail.com>
2020-10-18 15:06:23 -0500

    follow button functionining as expected for each robot

--- src/FollowButton.js
+++ src/FollowButton.js
@@ -4,10 +4,16 @@ import "./App.css";
 
 function FollowButton() {
   const [follow, setFollow] = useState("Follow");
+  const [buttonStyle, setButtonStyle] = useState(
+    "robot-follow-button-unclicked"
+  );
 
   const handleClick = (e) => {
     e.preventDefault();
     follow === "Follow" ? setFollow("Following") : setFollow("Follow");
+    buttonStyle === "robot-follow-button-unclicked"
+      ? setButtonStyle("robot-follow-button-clicked")
+      : setButtonStyle("robot-follow-button-unclicked");
   };
 
   return (
@@ -15,7 +21,7 @@ function FollowButton() {
       <Button
         type="button"
         variant="primary"
-        className="robot-follow-button"
+        className={buttonStyle}
         onClick={handleClick}
       >
         {follow}
