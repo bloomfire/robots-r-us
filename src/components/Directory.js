400976a824e571560e664ac1c9d2b9774c4d51ad
VanshikaP <vanshikapundir92@gmail.com>
2020-10-19 10:42:26 -0700

    layout styling

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import Employee from './Employee';
 
 const Directory = ({data}) => {
     return (
-        <div className='directory-container'>
-            Employee Directory Component
+        <DirectoryContainer className='directory-container'>
             {data.map(d => {
                 return (
                     <Employee employeeData={d} />
                 )
             })}
-        </div>
+        </DirectoryContainer>
     )
 }
+
+const DirectoryContainer = styled.div`
+    display: flex;
+    width: 90%;
+    margin: 5% auto;
+    flex-flow: row wrap;
+    justify-content: center;
+`
 
 export default Directory
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Employee = ({employeeData}) => {
     return (
-        <div className='employee-card'>
+        <EmployeeCard className='employee-card'>
             {employeeData.first_name + ' ' + employeeData.last_name}
-        </div>
+        </EmployeeCard>
     )
 }
+const EmployeeCard = styled.div`
+    background: midnightblue;
+    color: aliceblue;
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 2%;
+    margin: 2%;
+    border: 1px solid midnightblue;
+    border-radius: 5px;
+    width: 30%;
+    max-width: 300px;
+    height: 300px;
+    :hover{
+        box-shadow: 5px 10px 5px rgba(0,255,0,0.4);
+    }
+`
 
 export default Employee;
