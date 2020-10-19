78362e18ff3d765de1aed2c067dfe41ad3dcda20
Franky Leyva <surefirefjl@gmail.com>
2020-10-19 10:08:29 -0600

    First of the CSS added

--- src/App.js
+++ src/App.js
@@ -4,22 +4,28 @@ import './App.css';
 import { render } from '@testing-library/react';
 
 function App() {
- function handleClick() {
-    console.log('this is:', this);
+ function handleClick(e) {
+  e.preventDefault();
+  //Button works but only on one follow.
+  var elem = document.getElementById("followButton");
+  if(elem.innerHTML === 'Follow'){
+    elem.innerHTML = 'Following'
+  }else
+    elem.innerHTML = 'Follow'
   }
     return (
       <div className="App">
       <h1>Robots-R-Us</h1>
       {data.map((robot, index) => 
-        <ul>
-          <ul key={index}> 
-          <img src={robot.avatar ? robot.avatar : null} alt=''></img>
-          <p>
-          <button>Follow</button>
-          </p>
-          <p>{robot.first_name} {robot.last_name} </p>
-            <p>{robot.title}</p>
-            <p>{robot.email}</p>
+        <ul class='robots'>
+          <ul class='robot' key={index}> 
+            <img id='avatar' src={robot.avatar ? robot.avatar : null} alt=''></img>
+            <p>
+            <button id='followButton' onClick={handleClick}>Follow</button>
+            </p>
+            <p id='name'>{robot.first_name} {robot.last_name} </p>
+            <p id='title'>{robot.title}</p>
+            <p id='email'>{robot.email}</p>
           </ul>
         </ul>
       )}
