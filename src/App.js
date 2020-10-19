2a019a11eedc0aa761737e86b1c0b28d383a26fe
Franky Leyva <surefirefjl@gmail.com>
2020-10-19 09:03:14 -0600

    Got basis components rendering

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {data.map((robot, index) => 
+        <ul>
+          <ul key={index}> {robot.first_name} {robot.last_name}
+      <p>{robot.title}</p>
+      <p>{robot.email}</p>
+          </ul>
+        </ul>
+      )}
     </div>
   );
 }
