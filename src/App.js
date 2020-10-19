56d448bbd1a3479ceb2a1c12982923cb3418658a
Franky Leyva <surefirefjl@gmail.com>
2020-10-19 09:32:11 -0600

    Avatars and follow button added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
-  return (
-    <div className="App">
+ function handleClick() {
+    console.log('this is:', this);
+  }
+    return (
+      <div className="App">
       <h1>Robots-R-Us</h1>
       {data.map((robot, index) => 
         <ul>
-          <ul key={index}> {robot.first_name} {robot.last_name}
-      <p>{robot.title}</p>
-      <p>{robot.email}</p>
+          <ul key={index}> 
+          <img src={robot.avatar ? robot.avatar : null} alt=''></img>
+          <p>
+          <button>Follow</button>
+          </p>
+          <p>{robot.first_name} {robot.last_name} </p>
+            <p>{robot.title}</p>
+            <p>{robot.email}</p>
           </ul>
         </ul>
       )}
