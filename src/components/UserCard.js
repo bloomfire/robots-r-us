9fe17b9d98f6ad00d6b122e84ced2752486cc45b
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-10-19 11:36:51 -0500

    Adds default avatar for robots who don't have one

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -5,7 +5,14 @@ const UserCard = ({ user }) => {
   return (
     <div className="userCard">
       <div>
-        <img className="avatar" src={user.avatar}></img>
+        <img
+          className="avatar"
+          src={
+            user.avatar
+              ? user.avatar
+              : "https://png2.cleanpng.com/sh/9fe542e39c6ea12bbc51a2c408ca9671/L0KzQYm3VcA6N6h1iZH0aYP2gLBuTfJmdpVqip91ZXXvcX73iPltcaEygp9vcomwc7F0kQV1baMygdV4boOwRbLtUvE2PWM1T6Y6NkKxSYS5U8E6O2g2TaQAOEW0RIi7UMI6P191htk=/kisspng-bender-leela-philip-j-fry-computer-icons-5af2a552074162.9323193715258514740297.png"
+          }
+        ></img>
       </div>
       <div>
         {`${user.first_name} ${user.last_name}`} <br></br>
