b8b28c0a3db9d0827fb4915a7f06368b2a874671
VanshikaP <vanshikapundir92@gmail.com>
2020-10-19 11:06:48 -0700

    employee card styling

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -2,16 +2,42 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Employee = ({employeeData}) => {
+    const avatarImg = employeeData.avatar ? employeeData.avatar : 'https://robohash.org/default';
     return (
         <EmployeeCard className='employee-card'>
-            {employeeData.first_name + ' ' + employeeData.last_name}
+            <EmployeeAvatar className='employee-avatar' src={avatarImg} alt={employeeData.avatar ? employeeData.avatar : 'default'} />
+            <EmployeeInfo className='employee-info'>
+                <FullName className='employee-name'>
+                    {employeeData.first_name + ' ' + employeeData.last_name}
+                </FullName>
+                {employeeData.title 
+                    ? <JobTitle className='job'>
+                        <JobHeader className='job-header'>Job: </JobHeader>
+                        {employeeData.title}
+                    </JobTitle>
+                    : ''
+                }
+                {employeeData.email
+                    ? <EmailAddress className='email'>
+                        <EmailHeader className='email-header'>Email: </EmailHeader>
+                        {employeeData.email}
+                    </EmailAddress>
+                    : ''
+                }
+            </EmployeeInfo>
+            <FollowButton className='follow-btn'>
+                {employeeData.following
+                    ? <Unfollow>Unfollow</Unfollow>
+                    : <Follow>Follow</Follow>
+                }
+            </FollowButton>
         </EmployeeCard>
     )
 }
 const EmployeeCard = styled.div`
     background: midnightblue;
     color: aliceblue;
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -23,8 +49,54 @@ const EmployeeCard = styled.div`
     max-width: 300px;
     height: 300px;
     :hover{
-        box-shadow: 5px 10px 5px rgba(0,255,0,0.4);
+        box-shadow: 5px 10px 5px rgba(0,255,0,0.6);
     }
 `
+const EmployeeAvatar = styled.img`
+    height: 100px;
+    width: 100px;
+`
+
+const EmployeeInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: space-around;
+`
+
+const FullName = styled.p`
+    font-weight: bold;
+    font-size: 1.6rem;
+    margin: 2%;
+`
+
+const JobTitle = styled.p`
+    margin: 2%;
+`
+
+const JobHeader = styled.span`
+    font-weight: bold;
+`
+
+const EmailAddress = styled.p`
+    margin: 2%;
+`
+
+const EmailHeader = styled.span`
+    font-weight: bold;
+`
+const FollowButton = styled.button`
+    margin: auto;
+    font-size: 1.2rem;
+    font-weight: bold;
+    width: 60%;
+`
+const Unfollow = styled.span`
+    color: red;
+`
+
+const Follow = styled.span`
+    color: green;
+`
+
 
 export default Employee;
