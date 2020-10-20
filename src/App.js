385a59c6e5b3d13e74fdf97a49043fd63ef47656
Philip Hamner <philiphamner@gmail.com>
2020-10-19 22:45:10 -0700

    Sort robots by last name.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,42 @@ import data from './data';
 import './App.css';
 import Robot from './Robot';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Robots-R-Us</h1>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      data: data
+    };
+    this.sortRobots = this.sortRobots.bind(this);
+  }
+
+  sortRobots(robotData){
+    let sortedData = robotData.sort((a, b) => (a.last_name > b.last_name) ? 1 : -1);
+    this.setState({
+      data: sortedData
+    })
+    // console.log(sorted);
+
+  }
+  componentDidMount() {
+    this.sortRobots(data)
+  }
 
 
-      {data.map(robot => <Robot robot={robot} key={robot.id}/>)}
 
-
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <h1>Robots-R-Us</h1>
+  
+  
+        {this.state.data.map(robot => <Robot robot={robot} key={robot.id}/>)}
+  
+  
+      </div>
+    );
+  }
 }
 
 export default App;
