70a7af690404aad6e6060b6f919de851a10fdf77
GJoker94 <ellisjr.griffin@gmail.com>
2020-10-20 15:24:52 -0500

    First commit made changes to app.js by turning app function into app class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import data from './data';
+// import data from './data';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+
+    }
+
+  }
+  render(){
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
+      <h1>Robots-R-Us What up 
+      </h1>
+    
       {/* {data.map(robot => ...)} */}
     </div>
   );
+}
 }
 
 export default App;
