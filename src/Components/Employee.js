09c1acb35b8f672dc57f7c75a87232aa930c761a
Kenslee Moy <Kensleemoy@u.boisestate.edu>
2020-10-20 14:14:14 -0600

    added placeholder avatar from https://pixabay.com/vectors/avatar-icon-placeholder-1577909/ and updated some styling for the avatars

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -2,12 +2,15 @@ import React from 'react';
 import data from '../data';
 import FollowButton from './FollowButton';
 import './Employee.css';
+import PlaceHolderAvatar from './placeholder-avatar.png';
 
 class Employee extends React.Component {
     render() {
         return <div className='Employee'>{data.map(robot =>
             <div className='Employee-card'>
-                <img className='avatar' src={robot.avatar} />
+                <div>
+                    <img className='avatar' src={robot.avatar || PlaceHolderAvatar} />
+                </div>
                 <h2>{robot.first_name} {robot.last_name}</h2>
                 <FollowButton />
                 <h3>{robot.title}</h3>
