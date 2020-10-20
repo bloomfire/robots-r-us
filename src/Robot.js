e43be796936725fdab9acdb216430915d38be028
Philip Hamner <philiphamner@gmail.com>
2020-10-19 21:33:49 -0700

    Workable css implemented.

--- src/Robot.js
+++ src/Robot.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const MainPhoto = styled.div`
-  // outline: 2px solid blue;
+const RobotProfile = styled.div`
+  border-radius: 25px;
+  border: 2px solid red;
+
   margin: 20px;
-  width: 100%;
-  color: red;
-  background-color: blue;
+  padding: 15px;
+  width: 250px;
+  height: 250px;
+  color: black;
+  background-color: #72C4B6;
+  float:left
+`;
+
+const RobotPhoto = styled.img`
+  border-radius: 100%;
+  border: 2px solid black;
+  // margin: 20px;
+  // padding: 15px;
+  // width: 50px;
+  // height: 50px;
 `;
 
 
@@ -14,15 +28,13 @@ function Robot(props) {
     console.log(props.robot)
     
     return (
-      <MainPhoto>
-      <div className="robot">
-        <h2>{props.robot.first_name} {props.robot.last_name}</h2>
-        <h3>Follow</h3>
-        <h3>{props.robot.title}</h3>
-        <h3>{props.robot.email}</h3>
-
-      </div>
-      </MainPhoto>
+      <RobotProfile className="robot">
+        <RobotPhoto src={props.robot.avatar}/>
+        <h3>{props.robot.first_name} {props.robot.last_name}</h3>
+        <h4>Follow</h4>
+        <h4>{props.robot.title}</h4>
+        <h4>{props.robot.email}</h4>
+      </RobotProfile>
 
     );
 }
