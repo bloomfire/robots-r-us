b00aba4ef944cdd703476e4cff9a2f983b1a0cc4
Philip Hamner <philiphamner@gmail.com>
2020-10-20 16:26:10 -0700

    Convert Robot.js from functional to stateful component.

--- src/Robot.js
+++ src/Robot.js
@@ -3,12 +3,11 @@ import styled from 'styled-components';
 
 const RobotProfile = styled.div`
   border-radius: 25px;
-  border: 2px solid red;
-
+  border: 1px solid red;
   margin: 20px;
   padding: 15px;
-  width: 250px;
-  height: 250px;
+  width: 275px;
+  height: 275px;
   color: black;
   background-color: #72C4B6;
   float:left
@@ -16,30 +15,33 @@ const RobotProfile = styled.div`
 
 const RobotPhoto = styled.img`
   border-radius: 100%;
-  border: 2px solid black;
+  border: 1Spx solid black;
   // margin: 20px;
   // padding: 15px;
   width: 100px;
   height: 100px;
 `;
 
-const MrSparkle = 'https://pbs.twimg.com/profile_images/3555009298/146c8cd1fca9ab61b0b1b86fad1d627f.jpeg';
+const defaultAvatarFry = 'https://pbs.twimg.com/profile_images/3555009298/146c8cd1fca9ab61b0b1b86fad1d627f.jpeg';
 
-function Robot(props) { 
-    console.log(props.robot)
-    
+class Robot extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      followed: false
+    };
+  }
+
+  render(){
     return (
       <RobotProfile className="robot">
-        <RobotPhoto src={props.robot.avatar ? props.robot.avatar : MrSparkle}/>
-        <h3>{props.robot.first_name} {props.robot.last_name}</h3>
-        {/* <button onClick={activateLasers}>
-          Activate Lasers
-        </button> */}
-        <h4>{props.robot.title}</h4>
-        <h4>{props.robot.email}</h4>
+        <RobotPhoto src={this.props.robot.avatar ? this.props.robot.avatar : defaultAvatarFry}/>
+        <h3>{this.props.robot.first_name} {this.props.robot.last_name}</h3>
+        <h4>{this.props.robot.title}</h4>
+        <h4>{this.props.robot.email}</h4>
       </RobotProfile>
-
     );
+  }
 }
 
 export default Robot;
