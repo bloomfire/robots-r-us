394b059e16b162f3d334a67e9551c8b7212c008f
marshall405 <marshall.slemp@gmail.com>
2020-10-21 18:03:29 -0500

    adds following functionality and styling to button

--- src/components/Robot.js
+++ src/components/Robot.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Robot({ robot }) {
+    const [following, setFollowing] = useState(false)
     const { first_name, last_name, email, title, avatar } = robot
+
+    const handleOnClick = () => {
+        setFollowing(!following)
+    }
     return (
         <div className="robot-container">
             <img src={avatar} alt={first_name} />
             <h3>{`${first_name} ${last_name} `}</h3>
-            <button>Follow</button>
+            <button onClick={handleOnClick} className={following ? 'following' : ''}>
+                {following ? 'Following' : 'Follow'}
+            </button>
             <h4> {title}</h4>
             <h5> {email}</h5>
         </div>
