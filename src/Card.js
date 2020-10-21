3aa02e90dc7271f72e3f0a4afb1e65ebe2bda261
Grace Lindelien <gracelindelien@gmail.com>
2020-10-21 16:48:34 -0700

    Add follow button

--- src/Card.js
+++ src/Card.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 function Card({ robotData }) {
   console.log(robotData)
+  var followStatus = 'unfollowed';
   return (
     <div className="card">
       <img className="avatar" src={robotData.avatar} alt={robotData.last_name}></img>
       <div className="name">{robotData.first_name} {robotData.last_name}</div>
-      <div>{robotData.title}</div>
-      <div>{robotData.email}</div>
+      <div className="title">{robotData.title}</div>
+      <div className="email">{robotData.email}</div>
+      <div id={followStatus} className="follow">Follow</div>
     </div>
   );
 };
