36e541df9d98507ffb9e8ee005b885dff31f5194
norrischebl <norrischebl@gmail.com>
2020-10-21 13:31:10 -0700

    Rendered robot card content

--- src/pages/Home/RobotCard.js
+++ src/pages/Home/RobotCard.js
@@ -2,9 +2,29 @@ import React from "react";
 import style from "./style.module.scss";
 
 export default ({ robot }) => {
+  const { first_name, last_name, avatar, title, email } = robot;
+
+  const renderAvatar = avatar => {
+    if (avatar) {
+      return <img className={style.robotAvatar} src={avatar} alt="profile" />;
+    } else {
+      return (
+        <img
+          className={style.robotAvatar}
+          src={require("./defaultImage.jpg")}
+          alt="profile"
+        />
+      );
+    }
+  };
+
   return (
     <div className={style.robotCard}>
-      <h1 className={style.robotName}>{robot.first_name}</h1>
+      {renderAvatar(avatar)}
+      <h1 className={style.robotName}>{first_name + last_name}</h1>
+      <button>Following</button>
+      <h2 className={style.robotTitle}>{title && title}</h2>
+      <h3 className={style.robotEmail}>{email}</h3>
     </div>
   );
 };
