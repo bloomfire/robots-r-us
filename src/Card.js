8ff976c4a221477294069c088a03880fef45be53
Grace Lindelien <gracelindelien@gmail.com>
2020-10-21 16:25:12 -0700

    Render each robot's info to its own card

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
-function Card() {
+function Card({ robotData }) {
+  console.log(robotData)
   return (
-    <div>Test</div>
+    <div className="card">
+      <img src={robotData.avatar} alt={robotData.last_name}></img>
+      <div>{robotData.first_name} {robotData.last_name}</div>
+      <div>{robotData.title}</div>
+      <div>{robotData.email}</div>
+    </div>
   );
 };
 
