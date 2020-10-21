c315157f854786dce5d92315de5ed2496c734c12
tien phan <tienphan.dev@gmail.com>
2020-10-21 17:56:30 -0400

    Mapped through robots after mounting, then deconstruct in robotcard

---
+++ src/RobotCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RobotCard = (props) => {
+    const {id, first_name, last_name, email, title, avatar} = props.robot;
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RobotCard
