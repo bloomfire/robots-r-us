6e44b708fb3656185dbf246fc09cdf9bf890b43e
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-10-21 18:46:08 -0500

    Added Card from react bootstrap to help visualize data better

--- src/components/EmployeeData.js
+++ src/components/EmployeeData.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import data from '../data';
-
+import { Card } from 'react-bootstrap';
 function EmployeeData() {
 	return (
 		<div>
 			{data.map((info) => {
 				return (
 					<div className="employeeData" key={info.id}>
-						{info.first_name}
-						{info.last_name}
+						<Card style={{ width: '20rem' }}>
+							{info.first_name}
+							{info.last_name}
+						</Card>
 					</div>
 				);
 			})}
