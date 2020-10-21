2da2741efa4fce6e5ac01a6910aeb605fdb6e525
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-21 12:14:33 -0700

    styled app, added media queries, and added toggle classes

--- src/components/SingleRobotComponent.js
+++ src/components/SingleRobotComponent.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
 const SingleRobotComponent = ({robot, followRobot}) => {
+   const defaultImg = 'https://robohash.org/providentperspiciatiset.bmp?size=100x100&set=set1'
 
    const toggleButtonText = robot.following ? 'following' : 'follow';
+   const toggleButtonClass = robot.following ? "btn following": "btn";
+   const toggleTitleClass = robot.title ? 'title': "title null-title"
+   const toggleImages = robot.avatar ? robot.avatar : defaultImg;
+   const toggleTitleText = robot.title ? robot.title : 'No tittle provided';
+   const toggleEmail = robot.email ? robot.email : 'No email provided';
+
+
    return (
      <div className="inner-wrapper">
-         <img src={robot.avatar} alt={robot.first_name}/>
+         <img src={toggleImages} alt={robot.first_name}/>
          <div className="button-wrapper">
-            <button onClick={() => followRobot(robot)}>{toggleButtonText}</button>
+            <button onClick={() => followRobot(robot)} className={toggleButtonClass}>{toggleButtonText}</button>
          </div>
-         <h2>{robot.title}</h2>
+         <h2 className={toggleTitleClass}>{toggleTitleText}</h2>
          <p>{`${robot.first_name} ${robot.last_name}`}</p>
-         <p>{robot.email}</p>
+         <p>{toggleEmail}</p>
      </div> 
    )
 }
