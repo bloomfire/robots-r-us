88ff6d83e6a02438e36aa759fbc22c5a3ea34c8f
mjmiguel <migueljmiller@gmail.com>
2020-10-21 15:19:56 -0700

    fix useeffect dependencies

--- src/App.js
+++ src/App.js
@@ -3,30 +3,12 @@ import data from './data';
 import './App.css';
 import EmployeeCard from './components/EmployeeCard';
 
-
-function App() {
-
-  const [robots, setRobots] = useState(null);
-  const [robotCards, setRobotCards] = useState(null);
-
-  // Get robots list from data on component mount
-  useEffect(() => {
-    if (!robots) {
-      // sort by last name before adding
-      let sortedData = data.sort((a, b) => a.last_name - b.last_name);
-      setRobots(sortedData);
-    }
-
-    if (robots && !robotCards) {
-      makeEmployeeCards(robots)
-    }
-  }, [])
-
   // add employee card components to robotcards to dynamically populate employee cards
-  const makeEmployeeCards = () => {
+  const makeEmployeeCards = (robots) => {
+    console.log('in make employees')
     const cards = []
-    if (robots) {
       robots.forEach(robot => {
+        console.log('robot ', robot)
         cards.push(
           <EmployeeCard
             id={robot.id}
@@ -37,9 +19,29 @@ function App() {
           />
         )
       });
-      setRobotCards(cards);
-    }
+      return cards;
   };
+
+function App() {
+
+  const [robots, setRobots] = useState(null);
+  const [robotCards, setRobotCards] = useState(null);
+
+  // Get robots list from data on component mount
+  useEffect(() => {
+      // sort by last name before adding
+      let sortedData = data.sort((a, b) => a.last_name - b.last_name);
+      setRobots(sortedData);
+  }, []);
+
+  // update robotCards after getting raw data from data
+  useEffect(() => {
+    if (robots && !robotCards) {
+      setRobotCards(makeEmployeeCards(robots));
+    }
+  }, [robots, robotCards])
+
+
 
   return (
     <div className="App">
