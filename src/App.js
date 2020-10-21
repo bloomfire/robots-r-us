bbf3568cedda258684a59e5f048b2cb046351ca2
mjmiguel <migueljmiller@gmail.com>
2020-10-21 15:11:59 -0700

    add hooks and makes cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import data from './data';
 import './App.css';
+import EmployeeCard from './components/EmployeeCard';
+
 
 function App() {
+
+  const [robots, setRobots] = useState(null);
+  const [robotCards, setRobotCards] = useState(null);
+
+  // Get robots list from data on component mount
+  useEffect(() => {
+    if (!robots) {
+      // sort by last name before adding
+      let sortedData = data.sort((a, b) => a.last_name - b.last_name);
+      setRobots(sortedData);
+    }
+
+    if (robots && !robotCards) {
+      makeEmployeeCards(robots)
+    }
+  }, [])
+
+  // add employee card components to robotcards to dynamically populate employee cards
+  const makeEmployeeCards = () => {
+    const cards = []
+    if (robots) {
+      robots.forEach(robot => {
+        cards.push(
+          <EmployeeCard
+            id={robot.id}
+            name={`${robot.first_name} ${robot.last_name}`}
+            title={robot.title}
+            email={robot.email}
+            avatar={robot.avatar}
+          />
+        )
+      });
+      setRobotCards(cards);
+    }
+  };
+
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      <div>
+        { robotCards || null }
+      </div>
     </div>
   );
 }
