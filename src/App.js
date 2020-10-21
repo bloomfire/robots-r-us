4697b7d2f2bb2f2bab57296b3feab1f7e83b19f6
Chris Lay <chrisklay93@gmail.com>
2020-10-21 19:44:30 -0400

    Added bootstrap to help with column organization

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import RobotProfileCard from './components/RobotProfileCard.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
-      {data.map(robot =>
-        <RobotProfileCard
-        id={robot.id}
-        name={robot.first_name+" "+robot.last_name}
-        email={robot.email}
-        title={robot.title}
-        avatar={robot.avatar}
-        />
-      )}
+      <Container>
+        <h1>Robots-R-Us</h1>
+        <Row>
+          {data.map(robot =>
+          <Col className="profile" sm={6} lg={3}>
+            <RobotProfileCard
+            id={robot.id}
+            name={robot.first_name+" "+robot.last_name}
+            email={robot.email}
+            title={robot.title}
+            avatar={robot.avatar}
+            />
+          </Col>
+          )}
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/RobotProfileCard.js
+++ src/components/RobotProfileCard.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 export default class RobotProfileCard extends Component {
   render() {
     return (
-        <div>
-        <img src={this.props.avatar} alt={this.props.name}/>
-
-        <h3>{this.props.name}</h3>
+        <div className="profile">
+          <img src={this.props.avatar} alt={this.props.name}/>
+          <h3>{this.props.name}</h3>
+          <h3>{this.props.title}</h3>
+          <h3>{this.props.email}</h3>
         </div>
     )
   }
