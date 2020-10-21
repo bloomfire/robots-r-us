e4b26ee071c294143c3e751e4adb0399aeda8216
marshall405 <marshall.slemp@gmail.com>
2020-10-21 17:43:09 -0500

    renders robot data with reusable robot component

--- src/components/Robot.js
+++ src/components/Robot.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-export default function Robot() {
+export default function Robot({ robot }) {
+    const { first_name, last_name, email, title, avatar } = robot
     return (
         <div>
-
+            <img src={avatar} alt={first_name} />
+            <h3>{`${first_name} ${last_name} `}</h3>
+            <button>Follow</button>
+            <h4> {title}</h4>
+            <h5> {email}</h5>
         </div>
     )
 }
--- src/containers/Robots.js
+++ src/containers/Robots.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 import data from '../data';
 
+import Robot from '../components/Robot'
 export default function Robots() {
     return (
         <div>
-
+            {
+                data.map(robot => <Robot robot={robot} />)
+            }
         </div>
     )
 }
