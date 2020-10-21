5735787d7fb70935d25097797b001e8cbea9dc13
tien phan <tienphan.dev@gmail.com>
2020-10-21 18:02:01 -0400

    Rendered robot card with default avatar

--- src/RobotCard.js
+++ src/RobotCard.js
@@ -5,7 +5,15 @@ const RobotCard = (props) => {
 
     return (
         <div>
-
+            <div key={id}>
+              ID# {id}
+            <img src={avatar ? avatar : "https://www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png"} alt='robot avatar'/>
+                <div>
+                  <p>{first_name}{last_name}</p>
+                </div>
+                  <p>{title ? title : "No title yet.."}</p>
+                  <p>{email ? email: "No email on file"}</p>
+            </div>
         </div>
     )
 }
