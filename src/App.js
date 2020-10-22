d4d1ef3914d22aafbc7b921ccdd3c0ce1667ddee
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:18:32 -0700

    Bring in the robot card to display initial rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import data from './data';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
+import RobotCard from "./components/card/card-robot";
 
 const App = () => {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
+
+      <RobotCard />
       {/* {data.map(robot => ...)} */}
     </div>
   );
-}
+};
 
 export default App;
