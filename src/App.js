bc0c9759a8e4bf07aecb6e734666d0d56ffcdbd0
FuChai Mead <fuchaimead@FuChais-MacBook-Pro.local>
2020-10-22 10:20:21 -0600

    final commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import data from './data';
-import React from 'react';
+import React, { useEffect }  from 'react';
 import Robot from './robot';
 import { sortBy } from 'lodash';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    // out of time, but one way of storing the which robots have been followed and unfollowed is to use local storage
+    // basically stringify the data and add a property of isFollowing which then can be set to true or false
+    // this would be manipuled in the robot.js file
+    localStorage.setItem('data', JSON.stringify(data))
+  });
+
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
+      <h1 style={{color: 'white'}}>Robots-R-Us</h1>
       <div style={{display: 'flex', flexWrap: 'wrap'}}>
       {sortBy(data, ['last_name']).map(robot =>
-        <Robot {...robot} />
+        <Robot {...robot} key={robot.id}/>
         )}
       </div>
     </div>
--- src/robot.js
+++ src/robot.js
@@ -4,7 +4,6 @@ import { isNil } from 'lodash';
 
 const Robot = props => {
   const [isFollowing, setIsfollowing] = useState(false);
-
   const handleFollow = () => {
     setIsfollowing(!isFollowing);
   }
@@ -12,7 +11,7 @@ const Robot = props => {
   const following = isFollowing ? 'Following' : 'Follow';
   return <>
     <div className='card'>
-      <img src={isNil(props.avatar) ? '//robohash.org/4TO.png?set=set3&size=100x100' : props.avatar} alt={props.first_name + props.last_name}/>
+      <img src={isNil(props.avatar) ? '//robohash.org/4TO.png?set=set3&size=100x100' : props.avatar} alt={props.first_name + props.last_name} />
       <h3>{props.first_name} {props.last_name}</h3>
       <button onClick={handleFollow} className={following}>{following}</button>
       <h4>{props.title}</h4>
