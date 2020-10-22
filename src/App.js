0c51f898c62ec5b74b355170ac3f542a47c4b5f4
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:46:28 -0700

    Add Flexbox css styles wrapper to RobotCard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+import RobotCard from "./components/card/card-robot";
+
 import data from "./data";
 import "./App.css";
-import RobotCard from "./components/card/card-robot";
 
 const App = () => {
   const [robotData] = useState(data);
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {robotData.map((robot) => (
-        <RobotCard
-          key={robot.id}
-          firstName={robot.first_name}
-          lastName={robot.last_name}
-          avatar={robot.avatar}
-          title={robot.title}
-          email={robot.email}
-        />
-      ))}
+      <RobotCardWrapper>
+        {robotData.map((robot) => (
+          <RobotCard
+            key={robot.id}
+            firstName={robot.first_name}
+            lastName={robot.last_name}
+            avatar={robot.avatar}
+            title={robot.title}
+            email={robot.email}
+          />
+        ))}
+      </RobotCardWrapper>
     </div>
   );
 };
+
+const RobotCardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
 
 export default App;
