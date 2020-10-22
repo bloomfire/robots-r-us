0a423b7f50063818bb1580ff6b5481b9629a60c7
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:42:49 -0700

    Get initial styles for RobotCard

--- src/components/card/card-robot.js
+++ src/components/card/card-robot.js
@@ -1,18 +1,44 @@
 import React from "react";
+import styled from "styled-components"
+
 import Avatar from "../avatar";
 
 const RobotCard = ({ firstName, lastName, email, title, avatar }) => {
   return (
-    <div>
+    <CardContainer>
       <Avatar avatarUrl={avatar} avatarAlt={`${firstName} ${lastName}'s Avatar`} />
       <h2>
         {firstName} {lastName}
       </h2>
-      <button>Follow</button>
+      <StyledButton>Follow</StyledButton>
       <h3>{title}</h3>
       <p>{email}</p>
-    </div>
+    </CardContainer>
   );
 };
+
+const CardContainer = styled.div`
+  border-radius: 4px;
+  width: 300px;
+  margin: 24px;
+  padding: 32px;
+  background-color: #f7f7f7;
+`;
+
+const StyledButton = styled.button`
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.12), 0 2px 2px rgba(0, 0, 0, 0.12);
+  border: none;
+  font-size: 16px;
+  padding: 8px 16px;
+  width: 125px;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
 
 export default RobotCard;
