0f9312250e8a69dbe2aaa7bb8490844a21822167
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:18:10 -0700

    Build out the skeleton card for RobotCard Built the skeleton card based off the inspiration designs in the Repo

---
+++ src/components/card/card-robot.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RobotCard = () => {
+  return (
+    <div>
+      <img src="" alt="Full Name's Avatar" />
+      <h2>Full Name</h2>
+      <button>Follow</button>
+      <h3>Title</h3>
+      <p>email</p>
+    </div>
+  );
+};
+
+export default RobotCard;
