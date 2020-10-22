61ad3a9ade0565db6fa5b324aa58c1e03613549a
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:38:49 -0700

    Add initial styling for Avatar Component

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 const defaultAvatar = "https://robohash.org/autautet.bmp?size=100x100&set=set2";
 
 const Avatar = ({ avatarUrl, avatarAlt }) => (
-  <div>
-    <img src={avatarUrl ? avatarUrl : defaultAvatar} alt={avatarAlt} />
-  </div>
+  <AvatarContainer>
+    <StyledImage src={avatarUrl ? avatarUrl : defaultAvatar} alt={avatarAlt} />
+  </AvatarContainer>
 );
+
+const AvatarContainer = styled.div`
+  position: relative;
+  width: 125px;
+  height: 125px;
+  border-radius: 50%;
+  background-color: grey;
+  overflow: hidden;
+  margin: 0 auto;
+`;
+
+const StyledImage = styled.img`
+  position: absolute;
+  bottom: 0;
+  right: 10%;
+`;
 
 export default Avatar;
