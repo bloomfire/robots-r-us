f7eccb2df54a2ab9a9ec44ec0fc681a5ac33471a
mjmiguel <migueljmiller@gmail.com>
2020-10-21 17:32:56 -0700

    add comment

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 const Button = ({ id }) => {
 
+// useState hook to track button state from local storage to persist after refresh
 const [followed, setFollowed] = useState((localStorage.getItem(id) || false))
 
 const onClick = () => {
