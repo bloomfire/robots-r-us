035734cfbfcc4eab7f7a71fd36d3fd8d5f3c3b0d
yasmin yazdi <yyazdi13@gmail.com>
2020-10-21 19:45:18 -0500

    dynamically displaying robots. added some flex styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from './data';
 import './App.css';
 
 function App() {
+  const handleFollow = (e) => {
+    
+  }
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      <div className="container">
+      {data.map(robot => 
+      <div className="wrapper">
+        <div className="card" style={{width: "14rem"}}>
+          <div className="img-wrapper"
+          style={{backgroundImage: `url( ${robot.avatar ? robot.avatar :  'https://www.jbrhomes.com/wp-content/uploads/blank-avatar.png'}`}}
+          >
+            {/* <img src={robot.avatar ? robot.avatar : '/> */}
+          </div>
+          <div className="card-body">
+            <h5 className="card-title">{robot.first_name} {robot.last_name}</h5>
+            <p className="card-text">{robot.title ? robot.title : '-'}</p>
+            <p className="card-text">{robot.email ? robot.email : '-'}</p>
+            <a href="#" className="btn btn-primary" onClick={handleFollow}>{robot.email ? 'Following' : 'Follow'}</a>
+          </div>
+        </div>
+      </div>
+        )}
+      </div>
     </div>
   );
 }
