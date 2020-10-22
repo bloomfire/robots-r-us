e85b77acc9342f0406b15ec801ce8452b0a55d96
robert-walsh-hrri <rwalsh@gmail.com>
2020-10-22 13:35:46 -0500

    CSS grid implemented

--- src/App.js
+++ src/App.js
@@ -4,9 +4,40 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div 
+      className="App" 
+    >
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
+          gap: "15px 15px",
+        }} 
+      >
+        {data.map(
+          (robot) => {
+            return (
+              <div 
+                id={robot.id}
+                style={{
+                  height: "200px",
+                  width: "200px",
+                  border: "1px solid black",
+                  display: "inline-block",
+                  margin: "10px"
+                }}
+              >
+                <img src={robot.avatar}></img><br></br>
+                {robot.first_name}&nbsp;{robot.last_name}<br></br>
+                {robot.email}<br></br>
+
+                {robot.title}
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
