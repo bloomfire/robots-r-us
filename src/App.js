d5252541621909349cb7dbec1b62d4b0c86d905c
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-10-22 13:02:59 -0700

    set up skeleton components based on mockup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import data from './data';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
     </div>
   );
 }
