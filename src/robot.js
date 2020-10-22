c0f72bd02918bbd250ebb8a82b5772fdbf5b5bbd
FuChai Mead <fuchaimead@FuChais-MacBook-Pro.local>
2020-10-22 09:58:09 -0600

    set default avatar

--- src/robot.js
+++ src/robot.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './robot.css';
+import { isNil } from 'lodash';
 
 const Robot = props => {
-  console.log(props);
+  const handleFollow = () => {
+    // to do
+  }
+
   return <div>
     <div className='card'>
-      <img src={props.avatar} alt={props.first_name + props.last_name}/>
+      <img src={isNil(props.avatar) ? '//robohash.org/4TO.png?set=set3&size=100x100' : props.avatar} alt={props.first_name + props.last_name}/>
       <h3>{props.first_name}{props.last_name}</h3>
-      <button>Follow</button>
+      <button onClick={handleFollow}>Follow</button>
       <h4>{props.title}</h4>
       <p>{props.email}</p>
     </div>
