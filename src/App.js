2bd2242bbba3434a6218733903ff60f7228db0a3
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2020-10-22 10:10:54 -0700

    Feat: Create flexbox for robot cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import data from './data';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
-      {data.map(robot => {
-        let robotName = robot.first_name + " " + robot.last_name;
-        return (
-          <div className="robot-card-container" key={robot.id} value={robotName}>
-            <div className="image-container">
-              <img src={robot.avatar}/>
+        <h1>Robots-R-Us</h1>
+        <div className="robot-images-container">
+        {data.map((robot) => {
+          let robotName = robot.first_name + " " + robot.last_name;
+          return (
+            <div
+              className="robot-card-container"
+              key={robot.id}
+              value={robotName}
+            >
+              <div className="image-container">
+                <img src={robot.avatar} />
+              </div>
+              <div className="robot-name">{robotName}</div>
+              <div className="follow-button-container">
+                <button className="follow-button">Follow</button>
+              </div>
+              <div className="robot-title">{robot.title}</div>
+              <div className="email">{robot.email}</div>
             </div>
-            <div className="robot-name">
-              {robotName}
-            </div>
-            <div className="follow-button-container">
-              <button className="follow-button">Follow</button>
-            </div>
-            <div className="robot-title">
-              {robot.title}
-            </div>
-            <div className="email">
-              {robot.email}
-            </div>
-          </div>
-        )
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
