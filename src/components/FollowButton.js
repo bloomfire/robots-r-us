de82395cf00f64b8a65f27343a24540e14574aef
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-10-21 19:12:57 -0500

    added event listener func to button.(shows e. in console)

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -2,9 +2,13 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 function FollowButton() {
+	const clickedFollow = (e) => {
+		e.preventDefault();
+		console.log(e);
+	};
 	return (
 		<div>
-			<Button variant="primary" value="follow">
+			<Button variant="primary" value="follow" onClick={clickedFollow}>
 				Follow
 			</Button>
 		</div>
