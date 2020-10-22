cad0b2338ee1c17599dda4a24c9a838d2b641063
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:33:26 -0700

    Add a default avatar avatar URL
    Pulled url from a different set of robohash robots

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+const defaultAvatar = "https://robohash.org/autautet.bmp?size=100x100&set=set2";
+
 const Avatar = ({ avatarUrl, avatarAlt }) => (
   <div>
-    <img src={avatarUrl} alt={avatarAlt} />
+    <img src={avatarUrl ? avatarUrl : defaultAvatar} alt={avatarAlt} />
   </div>
 );
 
