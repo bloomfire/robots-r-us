ff9b3bac266a134e8c0d018c58d07540bbb2da92
Grover Richardson <groverrichardson1987@gmail.com>
2020-10-22 10:17:16 -0700

    Fixed some semantics for robotData.

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import logo from '../src/assets/robots-r-us-logo.svg';
 import '../src/sass/style.sass';
 
 function App() {
-    let sortedData = data.sort((a, b) => (a.last_name > b.last_name ? 1 : -1));
+    let sortedRobotData = data.sort((robotA, robotB) =>
+        robotA.last_name > robotB.last_name ? 1 : -1
+    );
     return (
         <div className="App">
             <img src={logo} alt="Robots R Us Logo" className="logo" />
             <section className="card-list">
-                {sortedData.map((card, i) => {
+                {sortedRobotData.map((card, i) => {
                     return <Card card={card} id={i} />;
                 })}
             </section>
