26774ca22fbd0234f149c8cbfba450a53f1f5a8e
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 22:31:07 -0700

    Add some simple tests:
    Test to check for title
    Test to match snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,8 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { container, getByText } = render(<App />);
+  const title = getByText(/Robots-R-Us/i);
+  expect(title).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
