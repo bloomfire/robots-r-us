5cda8ea623f79ea643e7a5bca7b6fe6b2f058464
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-10-22 13:13:40 -0700

    basic unformatted data store returning all information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import EmployeeDirectory from './Components/EmployeeDirectory/EmployeeDirectory';
 
 function App() {
   return (
     <div className="App">
-      <h1>Robots-R-Us</h1>
+      <h1 className="Company_Name">Robots-R-Us</h1>
+      <EmployeeDirectory/>
     </div>
   );
 }
---
+++ src/Components/EmployeeDirectory/EmployeeDirectory.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import data from '../../data.js';
+import './EmployeeDirectory.css';
+
+export default class EmployeeDirectory extends Component {
+    render () {
+        return (
+            <section className="Employee_Info">
+                {data.map(robot => {
+                    return (
+                        <div className="Employee_Card" key={robot.id}>
+                            <img src={robot.avatar} alt="robot-profile"/>
+                            <p id="name">{robot.first_name} {robot.last_name}</p>
+                            <p id="occupation">{robot.title}</p>
+                            <p id="contact">{robot.email}</p>
+                        </div>
+                    );
+                })}
+            </section>
+        );
+    }
+}
