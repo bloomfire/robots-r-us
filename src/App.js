533ca5000c5533c07c3cc913078f10cb7030521f
ArthurKimGl19 <arthur.kim.fse@gmail.com>
2020-10-22 10:05:51 -0700

    Feature: Render robot cards onto page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {data.map(robot => {
+        let robotName = robot.first_name + " " + robot.last_name;
+        return (
+          <div className="robot-card-container" key={robot.id} value={robotName}>
+            <div className="image-container">
+              <img src={robot.avatar}/>
+            </div>
+            <div className="robot-name">
+              {robotName}
+            </div>
+            <div className="follow-button-container">
+              <button className="follow-button">Follow</button>
+            </div>
+            <div className="robot-title">
+              {robot.title}
+            </div>
+            <div className="email">
+              {robot.email}
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
