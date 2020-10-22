1a513df75dc74d822ae667f7167213b1a6c417aa
Grover Richardson <groverrichardson1987@gmail.com>
2020-10-22 10:12:39 -0700

    Sorted robot data.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import logo from '../src/assets/robots-r-us-logo.svg';
 import '../src/sass/style.sass';
 
 function App() {
+    let sortedData = data.sort((a, b) => (a.last_name > b.last_name ? 1 : -1));
     return (
         <div className="App">
             <img src={logo} alt="Robots R Us Logo" className="logo" />
             <section className="card-list">
-                {data.map((card, i) => {
+                {sortedData.map((card, i) => {
                     return <Card card={card} id={i} />;
                 })}
             </section>
