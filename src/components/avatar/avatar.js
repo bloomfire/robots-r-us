06c3ec99ad983338690436b7ed75739c7062766a
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 22:01:44 -0700

    Use color theme for avatar component
    Add some styles to the default avatar icon to have more contrast

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+import colors from "../../theme/colors";
+
 const defaultAvatar = "https://robohash.org/autautet.bmp?size=100x100&set=set2";
 
 const Avatar = ({ avatarUrl, avatarAlt }) => (
-  <AvatarContainer>
-    <StyledImage src={avatarUrl ? avatarUrl : defaultAvatar} alt={avatarAlt} />
+  <AvatarContainer noAvatar={avatarUrl === null}>
+    <StyledImage noAvatar={avatarUrl === null} src={avatarUrl ? avatarUrl : defaultAvatar} alt={avatarAlt} />
   </AvatarContainer>
 );
 
@@ -14,7 +16,7 @@ const AvatarContainer = styled.div`
   width: 125px;
   height: 125px;
   border-radius: 50%;
-  background-color: grey;
+  background-color: ${({ noAvatar }) => (noAvatar ? colors.white : colors.tan)};
   overflow: hidden;
   margin: 0 auto;
 `;
@@ -23,6 +25,8 @@ const StyledImage = styled.img`
   position: absolute;
   bottom: 0;
   right: 10%;
+  filter: ${({ noAvatar }) => noAvatar && "grayscale(100%)"};
+  opacity: ${({ noAvatar }) => (noAvatar ? 0.65 : 1)};
 `;
 
 export default Avatar;
--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,4 +1,4 @@
-export const colors = {
+const colors = {
   white: "#ffffff",
   offWhite: "#f7f7f7",
   black: "#3f3f44",
