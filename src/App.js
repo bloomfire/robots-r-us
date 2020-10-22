88876c4bd73961edc015e22534aa0bba08f0d43a
plaetzaw <plaetzaw@gmail.com>
2020-10-22 10:55:39 -0500

    Mapping through data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import data from './data';
 import './App.css';
 
 function App() {
+
+  let cardMarkup = data.map((robot) => (
+    <li>
+    key={robot.id}
+    name={robot.name}
+    </li>
+  ))
+
+
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {cardMarkup}
     </div>
   );
 }
