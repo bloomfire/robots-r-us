85658dab80551a6422776c24e540e51e8ee291d3
Rembrandt Reyes <reyes.rembrandt@gmail.com>
2020-10-21 21:29:42 -0700

    Map over the data
    Passing in the data to the RobotCard component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import "./App.css";
 import RobotCard from "./components/card/card-robot";
 
 const App = () => {
+  const [robotData] = useState(data);
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-
-      <RobotCard />
-      {/* {data.map(robot => ...)} */}
+      {robotData.map((robot) => (
+        <RobotCard
+          key={robot.id}
+          firstName={robot.first_name}
+          lastName={robot.last_name}
+          avatar={robot.avatar}
+          title={robot.title}
+          email={robot.email}
+        />
+      ))}
     </div>
   );
 };
