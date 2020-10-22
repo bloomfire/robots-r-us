b4728d7d920bba485067ec2c20462537abd6c262
Joshua Akan-Etuk <joshuaakanetuk@gmail.com>
2020-10-22 18:05:59 -0400

    Fix ls update

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
   }
 
   render() {
+    ls.set('follow', JSON.stringify(this.state.follow));
     return (
       <div className="App">
         <h2>Robots-R-Us</h2>
