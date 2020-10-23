ead39957e2ae0923bf77af340a731214752b1e3b
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-23 06:45:49 -1000

    add sorting by last_name to Robots component

--- src/components/Robots.js
+++ src/components/Robots.js
@@ -5,11 +5,12 @@ import data from '../data'
 const Robots = () => {
     return (
         <div className="robots">
-           {
-                data.map(robot => 
+            {
+            data
+                .sort((a,b) => (a.last_name > b.last_name) ? 1 : ((b.last_name > a.last_name) ? -1 : 0))
+                .map(robot => 
                     <RobotCard key={robot.id} robot={robot}/>
-                )
-            }
+            )}
         </div> 
     )
 }
