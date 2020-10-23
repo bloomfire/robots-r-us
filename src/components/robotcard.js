81418d4f41847fb67343dff60c9dda7cb79e6308
Luis Cristo <cristlui4@gmail.com>
2020-10-23 16:18:07 -0500

    added simple layout for robot card data

--- src/components/robotcard.js
+++ src/components/robotcard.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Robotcard() {
-  return <div>Robot Card</div>;
+  return (
+    <div>
+      <div>Avatar</div>
+      <div>name</div>
+      <button>follow</button>
+      <p>Title</p>
+      <p>email</p>
+    </div>
+  );
 }
 
 export default Robotcard;
