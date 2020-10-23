f88697e66a3f9cabf3b6b945edf28ea328a3c1d1
Crystal Ly <crystal.m.ly@gmail.com>
2020-10-23 14:26:09 -0500

    dynamic follow button

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import './FollowButton.css';
 
 class FollowButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { text: 'Follow' };
+        this.state = { text: 'Follow', buttonClass: 'Follow' };
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick() {
-        let text = this.state.text === 'Follow' ? 'Following' : 'Follow'
-        this.setState({ text: text })
+        let text = this.state.text === 'Follow' ? 'Following' : 'Follow';
+        let buttonClass = text;
+        this.setState({
+            text: text,
+            buttonClass: buttonClass,
+        })
     }
 
     render() {
         return (
             <button
                 onClick={this.handleClick}
-                style={{
-                    background: 'green',
-                }}
+                className={this.state.buttonClass}
             >
                 {this.state.text}
             </button>
