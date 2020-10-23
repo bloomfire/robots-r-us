40b6b57ca0a1d5afc6cff8531840a0782c62ade4
Cyrielle Meyrueix <cmeyrueix44@gmail.com>
2020-10-23 09:27:59 -0700

    Added a follow button and more card styling

---
+++ src/components/followButton.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default class Following extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            following: this.props.following,
+        }
+    }
+
+
+    render() {
+        return (
+            <Button onClick={() => {
+                !this.state.follow ? this.setState({
+                    following: true,
+                }, () => { }) : this.setState({
+                    following: false,
+                }, () => { })
+            }} variant="success" > { (this.state.following === false) ? "Follow" : "Following"}</Button >
+        )
+    }
+}
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "../App.css";
 import Card from 'react-bootstrap/Card';
+import FollowBtn from './followButton'
+import '../styling/card.css'
 
 export default class Profile extends React.Component {
 
@@ -8,8 +10,7 @@ export default class Profile extends React.Component {
         super(props);
 
         this.state = {
-            following: false,
-            unfollow: true
+            following: false
         }
     }
 
@@ -20,13 +21,14 @@ export default class Profile extends React.Component {
             <Card style={{ width: '18rem' }} bg="light" border="info" className="mb-2" >
                 <Card.Img variant="top" src={profile.avatar ? profile.avatar : "https://vignette.wikia.nocookie.net/kingdomheartsfanon/images/a/a9/Walle1.gif/revision/latest/scale-to-width-down/150?cb=20131108025542"} class="img-fluid img-thumbnail" />
                 <Card.Body>
-                    <Card.Title>{profile.first_name + " " + profile.last_name}</Card.Title>
-                    <Card.Text>
+                    <Card.Title className='card-title-text'>{profile.first_name + " " + profile.last_name}</Card.Title>
+                    <Card.Text className='card-text'>
                         {profile.title}
                     </Card.Text>
-                    <Card.Text>
+                    <Card.Text className='card-email'>
                         {profile.email}
                     </Card.Text>
+                    <FollowBtn following={this.state.following}/>
                 </Card.Body>
             </Card>
         )
