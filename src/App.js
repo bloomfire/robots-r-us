f2d218ee4a2638e553a53ad46b620f0bbff54b2f
asekito <email@email.com>
2020-10-23 13:05:42 -0700

    data rendered on page with basic following functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+import Employee from './Employee';
 
 function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {data.map(robot => (<Employee robot={robot}/>))}
     </div>
   );
 }
---
+++ src/Employee.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+
+const Employee = ({robot}) => {
+  const [following, setFollowing] = useState(false);
+
+  const handleFollow = (e) => {
+    e.preventDefault();
+    setFollowing(!following);
+  };
+
+  return (
+    <div className="avatar-profile">
+      <img src={robot.avatar} alt="robot avatar"/>
+      <div>
+        {`${robot.first_name} ${robot.last_name}`}
+      </div>
+      <button onClick={e => handleFollow(e)}>{following ? "Unfollow" : "Follow"}</button>
+      <div>
+        {robot.title}
+      </div>
+      <div>
+        {robot.email}
+      </div>
+    </div>
+  )
+};
+
+export default Employee;
