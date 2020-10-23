28324b8ad75568d80b5dc945e6b58eb7d058d1d6
Courtney Overby <brioverby@gmail.com>
2020-10-22 22:28:31 -0500

    imported useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 import "./App.css";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Card from “@material-ui/core/Card”;
+import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
@@ -19,16 +19,16 @@ function App() {
             <Grid>
               <Grid>
                 <Card>
-                <CardContent>
-                  <dl>
-                    <Avatar src={avatar} />
-                    <dt>{first_name}</dt>
-                    <dt>{last_name}</dt>
-                    <dt>{email}</dt>
-                    <dt>{title}</dt>
-                  </dl>
-                  <Button>Button</Button>
-                </CardContent>
+                  <CardContent>
+                    <dl>
+                      <Avatar src={avatar} />
+                      <dt>{first_name}</dt>
+                      <dt>{last_name}</dt>
+                      <dt>{email}</dt>
+                      <dt>{title}</dt>
+                    </dl>
+                    <Button>Button</Button>
+                  </CardContent>
                 </Card>
               </Grid>
             </Grid>
