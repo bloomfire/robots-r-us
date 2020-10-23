d4fb5ce1f387fe86af3ab52d138ef776bdd9f2b6
Luis Cristo <cristlui4@gmail.com>
2020-10-23 16:46:55 -0500

    added initial pseudo state to app and passed prop to robot card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import data from "./data";
 import "./App.css";
 import RobotCard from "./components/robotcard";
 import { PageTitle, RobotGrid } from "./components/styledcomponents";
 
 function App() {
+  const [robot, setRobotFollow] = useState();
+
+  //to be used to handle local storage
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+
   return (
     <div className="App">
       <PageTitle>Robots-R-Us</PageTitle>
       <RobotGrid>
-        {data.map((robot) => (
-          <RobotCard key={robot.id} {...robot} />
-        ))}
+        {data.map((robot) => {
+          const followCheck = true;
+
+          return <RobotCard key={robot.id} {...robot} follow={followCheck} />;
+        })}
       </RobotGrid>
     </div>
   );
