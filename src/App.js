286c330dff1f5386b7df01a0c6a4edba642a03f1
Crystal Ly <crystal.m.ly@gmail.com>
2020-10-23 13:20:23 -0500

    added grid layout for data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import data from './data';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    paddingLeft: '10px',
+    paddingRight: '10px',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
       {/* {data.map(robot => ...)} */}
+      {/* Display raw data first, in a grid */}
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+
+            </Paper>
+          </Grid>
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+
+            </Paper>
+          </Grid>
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+
+            </Paper>
+
+          </Grid>
+
+          <Grid item xs={3}>
+            <Paper className={classes.paper}>
+
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
