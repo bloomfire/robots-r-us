a785eff4b9cd9176a886ca382a7bb0119ee1ced5
Luis Cristo <cristlui4@gmail.com>
2020-10-23 16:34:56 -0500

    added initial styling for robotcard and avatar element

--- src/components/styledcomponents.js
+++ src/components/styledcomponents.js
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 
 export const PageTitle = styled.h1``;
-export const RobotCard = styled.div``;
-export const Avatar = styled.img``;
+export const RobotGrid = styled.div``;
+
+export const RobotCard = styled.div`padding: 1rem;
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: white;
+margin: 1rem 2rem;
+text-align: center;
+border-radius: 4px;
+width: 300px;
+height: 300px;
+
+}`;
+export const Avatar = styled.img`
+  background-color: #6699cc;
+  border-radius: 50%;
+  height: 100px;
+  width: 100px;
+  margin-bottom: 1rem;
+`;
 export const Name = styled.h2``;
 export const Button = styled.button``;
 export const Content = styled.p``;
