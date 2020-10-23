b239758e353e5f5c50d1bd8d4d68cd8815a3ef07
sz16900 <sz16900@my.bristol.ac.uk>
2020-10-23 06:28:00 -1000

    add follow button with toggle state

--- src/components/RobotCard.js
+++ src/components/RobotCard.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const RobotCard = (props) => {
+
+    // Lets use this hook to preserve state in our component
+    const [follow, setFollow] = useState(false)
     // lets destructure data for clarity
     const {first_name,
         last_name,
@@ -11,6 +14,9 @@ const RobotCard = (props) => {
         <div className="robot-card">
             <img className="robot-avatar" src={avatar ? avatar : 'https://icon-library.com/images/small-user-icon/small-user-icon-19.jpg'} alt='robot user icon'/>
             <h2>{`${first_name} ${last_name}`}</h2>
+            <button className={follow ? 'followed' : null} onClick={() => {
+                setFollow(!follow)
+            }}>{follow ? 'Following' : 'Follow'}</button>
             <h3>{title}</h3>
             <h3>{email}</h3>
         </div>
