d0c2c20c2781616112a48cded125c510e1c782d7
claire-labry <clairestjlabry@gmail.com>
2020-10-23 15:38:14 -0500

    rendering robot data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import data from './data';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+        {data.map(robot => (
+        <div key={robot.id} style={{ margin: '30px' }}>
+          <div>{`First Name: ${robot.first_name}`}</div>
+          <div>{`Last Name: ${robot.last_name}`}</div>
+          <div>{`Email: ${robot.email}`}</div>
+          <div>{`Title: ${robot.title}`}</div>
+          <img src= {robot.avatar} />
+        </div>
+      ))}
     </div>
   );
 }
