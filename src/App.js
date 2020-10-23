bd6875c0974bf0b2b77484fa0b05db79eb8cade0
Luis Cristo <cristlui4@gmail.com>
2020-10-23 16:15:27 -0500

    added new skeleton components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import data from './data';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
+import RobotCard from "./components/robotcard";
 
 function App() {
   return (
     <div className="App">
       <h1>Robots-R-Us</h1>
-      {/* {data.map(robot => ...)} */}
+      {data.map((robot) => (
+        <RobotCard key={robot.id} />
+      ))}
     </div>
   );
 }
---
+++ src/components/robotcard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Robotcard() {
+  return <div>Robot Card</div>;
+}
+
+export default Robotcard;
