2b82acc091bf83b9d0b1defd6da5f16ae0c7844a
fbzr <fabricio.spb@gmail.com>
2020-10-26 14:19:40 -0700

    add follows state to app and handle follow button click

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "./data";
 // components
 import Card from "./components/Card";
@@ -6,12 +6,31 @@ import Card from "./components/Card";
 import "./App.css";
 
 function App() {
+  // chose Set as data structure due to its search efficiency
+  const [follows, setFollows] = useState(new Set());
+
+  const handleFollowClick = (id) => {
+    const newFollows = new Set(follows);
+
+    // delete id from set if value is found, otherwise add it to set
+    // delete method returns false if value is not found
+    !newFollows.delete(id) && newFollows.add(id);
+
+    // update state
+    setFollows(newFollows);
+  };
+
   return (
     <div className="App">
       <h1 className="title">Robots-R-Us</h1>
       <div className="cards-container">
         {data.map((robot) => (
-          <Card key={robot.id} profile={robot} />
+          <Card
+            key={robot.id}
+            profile={robot}
+            followed={follows.has(robot.id)}
+            toggleFollow={handleFollowClick}
+          />
         ))}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,16 @@ import React from "react";
 // style
 import "./Card.css";
 
-const Card = ({ profile }) => {
+const Card = ({ profile, followed, toggleFollow }) => {
   const { id, first_name, last_name, email, title, avatar } = profile;
 
   return (
     <div className="card">
       {avatar && <img src={avatar} alt={`${first_name} ${last_name}`} />}
       <h2>{`${first_name} ${last_name}`}</h2>
-      <button>Follow</button>
+      <button onClick={() => toggleFollow(id)}>
+        {followed ? "Following" : "Follow"}
+      </button>
       <h3>{title}</h3>
       <p>{email}</p>
     </div>
