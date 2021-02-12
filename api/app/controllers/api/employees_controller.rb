803fa138177f186d943f60fb412b9e16ed126db5
YoungTran <>
2021-02-11 22:39:18 -0800

    add query params filter

--- api/app/controllers/api/employees_controller.rb
+++ api/app/controllers/api/employees_controller.rb
@@ -2,9 +2,13 @@ module Api
   class EmployeesController < ActionController::Base
 
     def index
-      employees = Employee.all
-      render json: employees
+      @employees = Employee.all
+      @employees = Employee.filter_by_first_name(params_exist[:first_name]) if params_exist[:first_name]
+      @employees = Employee.filter_by_last_name(params_exist[:last_name]) if params_exist[:last_name]
+      render json: @employees
     end
-
+    def params_exist
+      params.permit(:id, :first_name, :last_name, :email, :title, :avatar)
+    end
   end
 end
--- api/app/models/employee.rb
+++ api/app/models/employee.rb
@@ -1,2 +1,4 @@
-class Employee < ApplicationRecord
+class Employee < ActiveRecord::Base
+  scope :filter_by_first_name, -> (first_name) { where first_name: first_name }
+  scope :filter_by_last_name, -> (last_name) { where last_name: last_name }
 end
