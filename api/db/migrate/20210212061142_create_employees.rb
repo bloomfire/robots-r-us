76450adfaac5e5a1bab04894098387dc0dda8109
YoungTran <>
2021-02-11 22:15:20 -0800

    update schema and migration

--- api/db/migrate/20210212061142_create_employees.rb
+++ api/db/migrate/20210212061142_create_employees.rb
@@ -1,10 +1,10 @@
 class CreateEmployees < ActiveRecord::Migration[6.1]
   def change
     create_table :employees do |t|
-      t.string :title
       t.string :first_name
       t.string :last_name
       t.string :email
+      t.string :title
       t.string :avatar
 
       t.timestamps
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_12_045622) do
+ActiveRecord::Schema.define(version: 2021_02_12_061142) do
 
   create_table "employees", force: :cascade do |t|
-    t.string "title"
     t.string "first_name"
     t.string "last_name"
     t.string "email"
+    t.string "title"
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
