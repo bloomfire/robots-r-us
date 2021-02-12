f3891bc618643ce2af0b2f3472244db26398c1c1
YoungTran <>
2021-02-11 21:31:17 -0800

    impl table column transformation in selector

--- src/data/selectors.js
+++ src/data/selectors.js
@@ -1,2 +1,24 @@
-export const getFormattedColumns = ({ data }) => [];
+function formatColumn(str) {
+  var i,
+    frags = str.split("_");
+  for (i = 0; i < frags.length; i++) {
+    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+  }
+  return frags.join(" ");
+}
+
+export const getFormattedColumns = ({ data }) => {
+  let formattedColumns = [];
+  if (data.length > 0) {
+    const columnJson = data[0];
+    for (const key in columnJson) {
+      formattedColumns.push({
+        Header: key === "id" ? key.toUpperCase() : formatColumn(key),
+        accessor: key.toLowerCase(),
+      });
+    }
+  }
+
+  return formattedColumns;
+};
 export const getFormattedData = ({ data }) => [];
--- src/library/RootComponent.jsx
+++ src/library/RootComponent.jsx
@@ -21,21 +21,19 @@
 
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Actions, Selectors } from "../data/index";
-import { data, fields, field_types } from '../data.json';
-
 // Import React Table
 import ReactTable from "react-table-6";
 import "react-table-6/react-table.css";
+import { Actions, Selectors } from "../data/index";
 
 const RootComponent = () => {
-  let formattedColumns = [];
-  let formattedData = [];
+  // let formattedColumns = [];
+  // let formattedData = [];
 
   // You can use format the data on the backend
   // or if you're familiar with Redux, you can do it in the selector...
-  // formattedColumns = useSelector(Selectors.getFormattedColumns);
-  // formattedData = useSelector(Selectors.getFormattedData);
+  const formattedColumns = useSelector(Selectors.getFormattedColumns);
+  const formattedData = useSelector(Selectors.getFormattedData);
 
   // or you can manipulate the hardcoded directly in the data.json file...
   // formatted Data = data.map(...);
@@ -51,10 +49,7 @@ const RootComponent = () => {
   return (
     <>
       <h1>Robots-R-Us</h1>
-      <ReactTable
-        columns={formattedColumns}
-        data={formattedData}
-      />
+      <ReactTable columns={formattedColumns} data={formattedData} />
     </>
   );
 };
