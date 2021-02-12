a32b2a4c1bc6aacc9fa1063f4eae76f3e620117c
YoungTran <>
2021-02-11 21:11:04 -0800

    impl employees controller return all using model

--- api/app/controllers/api/employees_controller.rb
+++ api/app/controllers/api/employees_controller.rb
@@ -2,8 +2,8 @@ module Api
   class EmployeesController < ActionController::Base
 
     def index
-      # IMPLEMENT ME
-      render json: File.read(File.expand_path('../src/data.json', Rails.root))
+      employees = Employee.all
+      render json: employees
     end
 
   end
